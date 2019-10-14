f4174fdc2943d4e801f5cc365d97fc179eca7e46
Steven Rouk <stevenrouk@gmail.com>
2019-10-14 10:56:40 -0600

    Create file of ArXiv sets.

---
+++ src/data/arxiv_sets.py
@@ -0,0 +1,25 @@
+# This set can be found through this API call (which can be accessed in a browser)
+# http://export.arxiv.org/oai2?verb=ListSets
+ARXIV_SETS = {
+    'cs': 'Computer Science',
+    'econ': 'Economics',
+    'eess': 'Electrical Engineering and Systems Science',
+    'math': 'Mathematics',
+    'physics': 'Physics',
+    'physics:astro-ph': 'Astrophysics',
+    'physics:cond-mat': 'Condensed Matter',
+    'physics:gr-qc': 'General Relativity and Quantum Cosmology',
+    'physics:hep-ex': 'High Energy Physics - Experiment',
+    'physics:hep-lat': 'High Energy Physics - Lattice',
+    'physics:hep-ph': 'High Energy Physics - Phenomenology',
+    'physics:hep-th': 'High Energy Physics - Theory',
+    'physics:math-ph': 'Mathematical Physics',
+    'physics:nlin': 'Nonlinear Sciences',
+    'physics:nucl-ex': 'Nuclear Experiment',
+    'physics:nucl-th': 'Nuclear Theory',
+    'physics:physics': 'Physics (Other)',
+    'physics:quant-ph': 'Quantum Physics',
+    'q-bio': 'Quantitative Biology',
+    'q-fin': 'Quantitative Finance',
+    'stat': 'Statistics'
+}
