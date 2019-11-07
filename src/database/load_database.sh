b29a1b37cccccb415f10c1c10be699dc6c2bae87
Steven Rouk <stevenrouk@gmail.com>
2019-11-07 13:27:03 -0700

    Create sh file to create the papers table from scratch.

---
+++ src/database/load_database.sh
@@ -0,0 +1,5 @@
+python src/database/create_database.py
+python src/database/drop_table.py drop-table --name papers
+python src/database/create_tables.py
+python src/database/insert_into_table.py
+python src/database/select_from_table.py
