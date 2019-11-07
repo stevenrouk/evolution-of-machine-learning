86d23d15327336efd8eb00f1d522867fa36345f2
Steven Rouk <stevenrouk@gmail.com>
2019-11-07 11:53:53 -0700

    Print if database already exists instead of throwing an exception.

--- src/database/create_database.py
+++ src/database/create_database.py
@@ -1,15 +1,19 @@
 import psycopg2
 from psycopg2 import sql
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT # <-- ADD THIS LINE
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from psycopg2.errors import DuplicateDatabase
 
 conn = psycopg2.connect(dbname='postgres',
-      user='postgres', host='localhost', port='5435')
+    user='postgres', host='localhost', port='5435')
 
-conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) # <-- ADD THIS LINE
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
 cur = conn.cursor()
 
 # Create database
-cur.execute(
-    sql.SQL("CREATE DATABASE {}").format(sql.Identifier('capstone2'))
+try:
+    cur.execute(
+        sql.SQL("CREATE DATABASE {}").format(sql.Identifier('capstone2'))
     )
+except DuplicateDatabase:
+    print('database already exists')
