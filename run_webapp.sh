895baa20dd282b91488563edbdc6fd909c1b6d0f
Steven Rouk <stevenrouk@gmail.com>
2019-11-13 16:48:50 -0700

    Add web app files.

--- run_webapp.sh
+++ run_webapp.sh
@@ -1,2 +1,4 @@
+# Reference: https://pyliaorachel.github.io/blog/tech/system/2017/07/07/flask-app-with-gunicorn-on-nginx-server-upon-aws-ec2-linux.html
 # Reference: https://www.digitalocean.com/community/tutorials/how-to-serve-flask-applications-with-gunicorn-and-nginx-on-centos-7
+# Reference: https://www.digitalocean.com/community/tutorials/how-to-install-nginx-on-centos-7
 gunicorn --bind 0.0.0.0:8080 --chdir app/ wsgi:app
